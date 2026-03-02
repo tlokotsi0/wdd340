61f06974ba8960a19dc2736bb7f1b497b479f90b
tlokotsi0 <amantusf@gmail.com>
2026-03-02 22:03:37 +0200

    Week 1 Lessons

--- server.js
+++ server.js
@@ -6,14 +6,31 @@
  * Require Statements
  *************************/
 const express = require("express")
+expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expressEjsLayouts = require("express-ejs-layouts")
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+/* ***********************
+ * Index route
+ *************************/
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
+
+/* ********************************
+ * View Engine and Templates
+ *********************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") 
+
 
 /* ***********************
  * Local Server Information
